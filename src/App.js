bb4c8f0f0c332f64aa7b6e598beb596847b5f91c
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2022-06-18 17:24:01 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//SASS
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, //kind of file extension this rule should look for and apply in test
+        exclude: /node_modules/, //folder to be excluded
+        use: "babel-loader", //loader which we are going to use
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
