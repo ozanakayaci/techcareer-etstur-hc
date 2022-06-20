db87cf7f2296e05645d1d3678db820977e26cfad
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2022-06-20 16:41:10 +0300

    main components created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 
+//components
+import Navbar from "./components/Navbar/Navbar.js";
+import MainMenu from "./components/MainMenu/MainMenu.js";
+import EventPage from "./components/EventPages/EventPage.js";
+
 //SASS
 import "./App.scss";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
+      <MainMenu />
+      <EventPage />
       <h1>Hello</h1>
     </div>
   );
