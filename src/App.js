9b302c08368848ff2581c0e8094be14221f29c87
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2022-06-20 22:46:41 +0300

    rename component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom";
 
 //components
 import Navbar from "./components/Navbar/Navbar.js";
-import MainMenu from "./components/MainMenu/MainMenu.js";
+import Homepage from "./components/Homepage/Homepage.js";
 import EventPage from "./components/EventPages/EventPage/EventPage.js";
 import Events from "./components/EventPages/Events/Events.js";
 import Error404 from "./components/404/Error404.js";
@@ -18,7 +18,7 @@ const App = () => {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<MainMenu />} />
+        <Route path="/" exact element={<Homepage />} />
         <Route path="eventPage">
           <Route path=":event_id" element={<EventPage />} />
         </Route>
