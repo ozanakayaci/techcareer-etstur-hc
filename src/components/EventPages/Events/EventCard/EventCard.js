573d96a2896ed56d4c52a4c9956cc884e8bc6ce8
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2022-06-22 20:00:36 +0300

    events search page update

--- src/components/EventPages/Events/EventCard/EventCard.js
+++ src/components/EventPages/Events/EventCard/EventCard.js
@@ -4,8 +4,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function EventCard(item) {
+  const date = `${new Date(item.props.date)}`;
   return (
-    <Link to={`/eventPage/${item.props.event_id}`}>{item.props.name}</Link>
+    <div className="cards">
+      <Link to={`/eventPage/${item.props.event_id}`}>
+        <div className="box">
+          <div className="date-box">
+            <div className="day">{date.slice(8, 10)}</div>
+            <div>
+              <div>{date.slice(0, 3)}</div>
+              <div>{date.slice(4, 7)}</div>
+              <div>{date.slice(11, 15)}</div>
+            </div>
+          </div>
+          <div className="nlc">
+            <div>{item.props.name}</div>
+            <Link to={`/events/location/${item.props.location.name}`}>
+              <div className="faint">{item.props.location.name}</div>
+            </Link>
+            <div>
+              <span>{item.props.category.slice(0, 1).toUpperCase()}</span>
+              <span className="faint">
+                {item.props.category.slice(1, item.props.category.length)}
+              </span>
+            </div>
+          </div>
+          <div className="mini-img">
+            <img src={item.props.mini_image} alt="" />
+          </div>
+        </div>
+      </Link>
+    </div>
   );
 }
 
--- src/components/EventPages/Events/Events.js
+++ src/components/EventPages/Events/Events.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 //componenet
 import EventCard from "./EventCard/EventCard.js";
 
+//css
+import "./Events.scss";
 //redux
 import { useParams } from "react-router-dom";
 
@@ -11,6 +14,8 @@ import data from "../../../../data.json";
 
 function Events() {
   let { category, location, word, date_range } = useParams();
+
+  const [filteredEvents, setFilteredEvents] = useState([]);
 
   let filterWord = "";
 
@@ -23,34 +28,178 @@ function Events() {
     ? (filterWord = date_range)
     : (filterWord = word);
 
-  console.log(filterWord);
-
-  const filteredEvents = [...data].filter((item) => {
-    if (filterWord != undefined) {
-      return Object.keys(item).some(
-        (prop) =>
-          item[prop] !== undefined &&
-          (item[prop]
-            .toString()
-            .toLowerCase()
-            .includes(filterWord.toLowerCase()) ||
-            Object.keys(item[prop]).some(
-              (prop2) =>
-                item[prop][prop2] !== undefined &&
-                item[prop][prop2]
-                  .toString()
-                  .toLowerCase()
-                  .includes(filterWord.toLowerCase())
-            ))
-      );
-    } else {
-      return [...data];
+  let handleFilter = (datas, key, name) => {
+    setFilteredEvents(
+      [...datas].filter((item) => {
+        if (key != undefined) {
+          return Object.keys(item).some(
+            (prop) =>
+              item[prop] !== undefined &&
+              (item[prop]
+                .toString()
+                .toLowerCase()
+                .includes(key.toLowerCase()) ||
+                Object.keys(item[prop]).some(
+                  (prop2) =>
+                    item[prop][prop2] !== undefined &&
+                    item[prop][prop2]
+                      .toString()
+                      .toLowerCase()
+                      .includes(key.toLowerCase())
+                ))
+          );
+        } else {
+          return [...datas];
+        }
+      })
+    );
+    if (datas.length == 0) {
+      handleFilter(data, key, name);
     }
-  });
+  };
+
+  useEffect(() => {
+    handleFilter(data, filterWord);
+  }, [filterWord]);
 
   console.log(filteredEvents);
   return (
     <div>
+      <div className="filters">
+        <h1>Filters</h1>
+        <div className="dropdown">
+          <button className="dropbtn">Categories</button>
+          <div className="dropdown-content">
+            <button
+              onClick={() => {
+                handleFilter(filteredEvents, "music", "category");
+              }}
+            >
+              Music
+            </button>
+            <button
+              onClick={() => {
+                handleFilter(filteredEvents, "theatre", "category");
+              }}
+            >
+              Theatre
+            </button>
+            <button
+              onClick={() => {
+                handleFilter(filteredEvents, "art", "category");
+              }}
+            >
+              Art
+            </button>
+            <button
+              onClick={() => {
+                handleFilter(filteredEvents, "sport", "category");
+              }}
+            >
+              Sport
+            </button>
+          </div>
+        </div>
+        <div className="dropdown">
+          <button className="dropbtn">Cities</button>
+          <div className="dropdown-content">
+            <button
+              onClick={() => {
+                handleFilter(filteredEvents, "antalya", "city");
+              }}
+            >
+              Antalya
+            </button>
+            <button
+              onClick={() => {
+                handleFilter(filteredEvents, "balıkesir", "city");
+              }}
+            >
+              Balıkesir
+            </button>
+            <button
+              onClick={() => {
+                handleFilter(filteredEvents, "istanbul", "city");
+              }}
+            >
+              Istanbul
+            </button>
+            <button
+              onClick={() => {
+                handleFilter(filteredEvents, "bilbao", "city");
+              }}
+            >
+              Bilbao
+            </button>
+            <button
+              onClick={() => {
+                handleFilter(filteredEvents, "werchter", "city");
+              }}
+            >
+              Werchter
+            </button>
+          </div>
+        </div>
+        <div className="dropdown">
+          <button className="dropbtn">Venue</button>
+          <div className="dropdown-content">
+            <button
+              onClick={() => {
+                handleFilter(
+                  filteredEvents,
+                  "Dorock XL Kadıköy, İstanbul",
+                  "name"
+                );
+              }}
+            >
+              Dorock XL Kadıköy, İstanbul
+            </button>
+            <button
+              onClick={() => {
+                handleFilter(filteredEvents, "ESTADIO SAN MAMÉS", "name");
+              }}
+            >
+              ESTADIO SAN MAMÉS
+            </button>
+            <button
+              onClick={() => {
+                handleFilter(filteredEvents, "Altınoluk Amfi Tiyatro", "name");
+              }}
+            >
+              Altınoluk Amfi Tiyatro
+            </button>
+            <button
+              onClick={() => {
+                handleFilter(filteredEvents, "Trump Sahne İstanbul", "name");
+              }}
+            >
+              Trump Sahne İstanbul
+            </button>
+            <button
+              onClick={() => {
+                handleFilter(
+                  filteredEvents,
+                  "Antalya Açıkhava Tiyatrosu",
+                  "name"
+                );
+              }}
+            >
+              Antalya Açıkhava Tiyatrosu
+            </button>
+            <button
+              onClick={() => {
+                handleFilter(
+                  filteredEvents,
+                  "Rock Werchter Festivalpark",
+                  "name"
+                );
+              }}
+            >
+              Rock Werchter Festivalpark
+            </button>
+          </div>
+        </div>
+      </div>
       {
         //filtered by
         location != undefined ? (
